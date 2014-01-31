1f6592ef411b737ab0ec6ea0bab4d607c15b4f8c
Ben Lewis <bennlewis@gmail.com>
2014-01-31 10:10:26 -0700

    Initialized project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlantingSeasonCoordinator::Application.config.secret_key_base = '33062fc690ecde80bfec2b63bdd70ef2e3a550026723554f824c7b80f9c7ce91e08d45360a912982e1efc1df1efbe5d65a9a8a0b330b63d9d2963445b79c33a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PlantingSeasonCoordinator::Application.config.session_store :cookie_store, key: '_planting_season_coordinator_session'
