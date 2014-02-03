f35f845f91435b7c60bfa53b2c6c9f8c38c30b4b
Ben Lewis <bennlewis@gmail.com>
2014-02-03 16:40:41 -0700

    built up the view and added the form

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,4 @@
+$('#add-to-garden').click(function(e) {
+  e.preventDefault();
+  alert("UR MOM");
+})
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    @bed = "Tomato Bed Info"
+    @bed = JSON.parse("{\"id\":1,\"name\":\"Tomatoes\",\"garden_id\":1,\"width\":3,\"depth\":2,\"created_at\":\"2014-01-30T18:06:29.733Z\",\"updated_at\":\"2014-02-03T22:53:32.695Z\",\"notes\":\"Plant cover crop after harvest.\"}")
+    @width = @bed["width"]
+    @depth = @bed["depth"]
   end
 end
