cce402ac52342daa4213bd6d31800b515e0a9ec6
Tyler Long <tyler.stephen.long@gmail.com>
2014-02-05 15:04:15 -0700

    WIP, bedscontrollerspec

---
+++ spec/controllers/bed_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe BedsController do
+  let(:params) { { :bed => {     :name  => "HERBS!!",
+                                 :notes => "These are all the herbs!",
+                                 :zipcode => "80210",
+                                 :width => 5,
+                                 :depth => 5 } } }
+
+  it "creates a bed" do
+    cookies.signed[:user_id] = 1
+    VCR.use_cassette "controller_create_a_new_bed" do
+      post :create, params
+      expect(response.status).to eq(200)
+      expect(JSON.parse(response.body)["name"]).to eq "Herb Garden Bed"
+    end
+  end
+end
