2a28f8be02307aef984a6806b5ac5ab59ec0d4b7
Luke Martinez <lukemartinez@gmail.com>
2014-02-05 14:55:08 -0700

    first feature spec passing, bed attributes created

--- app/controllers/beds_controller.rb
+++ app/controllers/beds_controller.rb
@@ -1,5 +1,10 @@
 class BedsController < ApplicationController
+
   def new
     @bed = Bed.new
+  end
+
+  def create
+    redirect_to :back
   end
 end
--- app/models/bed.rb
+++ app/models/bed.rb
@@ -1,2 +1,27 @@
-class Bed 
+class Bed
+
+  attr_accessor :id, :name, :zipcode, :notes, :width, :depth
+
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+ 
+  def persisted?
+    false
+  end
+
+  def self.create
+
+  end
+
+  def initialize(data = {})
+    @id      = data["id"]
+    @name    = data["name"]
+    @zipcode = data["zipcode"]
+    @notes   = data["notes"]
+    @depth   = data["depth"]
+    @width   = data["width"]
+  end
+
+
+
 end
