53aded07550a0e87189b6a5429034a1335a37666
Ben Lewis <bennlewis@gmail.com>
2014-02-05 13:39:10 -0700

    undo change to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 PlantingSeasonCoordinator::Application.routes.draw do
-  scope "my_gardens" do
+  scope "my-gardens" do
     root "welcome#index"
     post "/", to: "welcome#create", as: :post_bed_form
   end
