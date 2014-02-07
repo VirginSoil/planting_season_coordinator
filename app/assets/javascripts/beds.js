2215c2df7cdfa87c103c4d7967c1311436412a8f
Ben Lewis <bennlewis@gmail.com>
2014-02-07 10:39:36 -0700

    setup logic in JS for different kinds of bed squares

--- app/assets/javascripts/beds.js
+++ app/assets/javascripts/beds.js
@@ -8,7 +8,7 @@ $(function(){
       var thisOne = parseId($(allTheSquares[i]).attr('id'));
       for (var j = 0; j < takenSpots.length; j++) {
         if (arraysIdentical(takenSpots[j], thisOne)) {
-          $(allTheSquares[i]).attr("class", 'square-foot active planted');
+          $(allTheSquares[i]).attr("class", 'square-foot planted');
           $(allTheSquares[i]).append('<img src="http://www.placekitten.com/50/50">');
         }
       }
@@ -39,6 +39,20 @@ $(function(){
     showNewPlantPanel();
   });
 
+  $('td').click(function(e){
+    var element = $(e.currentTarget);
+    var thisClass = $(this).attr("class");
+    if ( thisClass == 'square-foot planted') {
+      selectPlanted(element);
+    } else if (thisClass == 'square-foot active planted') {
+      deselectPlanted(element);
+    } else if (thisClass == 'square-foot active') {
+      deselectUnplanted(element);
+    } else {
+      selectUnplanted(element);
+    }
+  });
+
   function showBedInfoPanel() {
     $('.plant-actions').css("display", "none");
     $('.bed-information').css("display", "inline");
@@ -66,20 +80,29 @@ $(function(){
     $('li#actions a').css("background-color", "#bbb");
   };
 
-  $('td').click(function(e){
-    var element = $(e.currentTarget);
-    if ($(this).attr("class") == 'square-foot active planted') {
-      element.attr('class', 'square-foot active');
-      element.css('background-color', 'green');
-      showPlantActionsPanel();
-    } else if ($(this).attr("class") == 'square-foot active') {
-      element.attr('class', 'square-foot');
-      element.css('background-color', 'white');
-      showBedInfoPanel();
-    } else {
-      element.attr('class', 'square-foot active');
-      element.css('background-color', 'green');
-      showNewPlantPanel();
-    }
-  });
+  function selectPlanted(element) {
+    element.attr('class', 'square-foot active planted');
+    element.css('background-color', 'green');
+    showPlantActionsPanel();
+  };
+
+  function deselectPlanted(element) {
+    element.attr('class', 'square-foot planted');
+    element.css('background-color', 'white');
+    showBedInfoPanel();
+  };
+
+  function selectUnplanted(element) {
+    element.attr('class', 'square-foot active');
+    element.css('background-color', 'green');
+    showNewPlantPanel();
+  };
+
+  function deselectUnplanted(element) {
+    element.attr('class', 'square-foot');
+    element.css('background-color', 'white');
+    showBedInfoPanel();
+  }
+
+
 });
