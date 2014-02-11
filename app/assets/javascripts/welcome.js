b676f42c2294f9f09f6deb40188617ac22648257
Nathaniel Watts <reg@nathanielwatts.com>
2014-02-11 09:57:44 -0700

    add basic styling

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,4 +1,13 @@
 $(function() {
+
+  // resize cells
+  var cellWidth =$("#bed-grid-panel td").width();
+  $("#bed-grid-panel td").height(cellWidth);
+  $(window).resize(function() {
+    var cellWidth =$("#bed-grid-panel td").width();
+    $("#bed-grid-panel td").height(cellWidth);
+  });
+
   $('#add-to-garden').on("click", function(e) {
     var thisSquare, thisSquareId, x, y, plant, bed_id, queryData;
 
